0f6afabe5e7e7faedf6150db8a1d5f85f37e4cb0
Arieloto <arielpizarrof@gmail.com>
2023-09-14 14:42:42 -0300

    primera

---
+++ main.js
@@ -0,0 +1,41 @@
+
+/*let num1 = parseInt(prompt("ingresa 1° numero"))
+let operacion = prompt("Ingrese operacion")
+let num2 = parseInt(prompt("ingresa 2° numero"))
+
+if(operacion == "+")
+{
+    console.log(num1 + num2)
+}
+else if (operacion == "-")
+{
+    console.log(num1 - num2)
+}
+else if (operacion == "*")
+{
+    console.log(num1 * num2)
+}
+else if (operacion == "/")
+{
+    console.log(num1 / num2)
+}
+else 
+{
+    console.log("Ingrese Operacion Valida")
+    alert("Ingrese Operacion Valida")
+}*/
+
+
+let ciclo=false
+while(ciclo==false){
+    ciclo=false
+let num1 = parseInt(prompt("ingresa 1° numero"))
+let operacion = prompt("Ingrese operacion")
+let num2 = parseInt(prompt("ingresa 2° numero"))
+ciclo=true
+if(operacion == "+")
+    {console.log(num1 + num2)
+     alert(num1+num2)
+     ciclo=false
+    }
+}

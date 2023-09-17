d2e03c5237de3e478bfc4e63ffac853d3e8bf8f9
jas0307 <mtx86@hotmail.com>
2023-09-17 16:50:47 -0300

    Terminado

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 const calcParametro = (valor1, valor2, calculo) => {
     switch (calculo) {
         case '1':
-            return valor1 * valor2 ; //Calculo de voltaje V=I*R
+            return valor1 * valor2 ; //Calculo de Voltaje V=I*R
         case '2':
-            return valor1 / valor2 ;//Calculo de resistencia R=V/I
+            return valor1 / valor2 ;//Calculo de Resistencia R=V/I
         case '3':
-            return valor1 / valor2 ;//Calculo de amperaje I=V/R
+            return valor1 / valor2 ;//Calculo de Amperaje I=V/R
         default:
-            return alert("Calculo no válido") //Opcio no valida
+            return alert("Calculo no válido") //Opcion no valida
     }
 };
 
@@ -22,37 +22,51 @@ const menu = () => {
     while (true) {
         const opcion = prompt("Elija parametro a calcular: 1 (Voltaje) 2 (Resistencia) 3 (Corriente) 4 (Salir):");
 
-        if (opcion === '1') { //opcion Voltaje
+        if (opcion === '1') { //Opcion Voltaje
+            
             const valor1 = parseFloat(prompt("Ingrese valor de corriente:"));
-            const valor2 = parseFloat(prompt("Ingrese valor de resistencia:"));
+            const valor2 = parseFloat(prompt("Ingrese valor de resistencia:"));            
+            if (isNaN(Number(valor1)) || isNaN(Number(valor2))) {//valor es un numero
+                alert("Al menos uno de los valores es incorrecto");
+                
+             } else{      
             
             const resultado = calcParametro(valor1, valor2 , opcion);
             console.log("El resultado es: resultado" + resultado)
-            alert("El voltaje calculado es  " +  resultado + "V")
+            alert("El voltaje calculado es  " +  resultado + "(V)")
+             }
         }
-        else if( opcion === '2' ) {//opcion Voltaje
+        else if( opcion === '2' ) {//opcion Resistencia
             const valor1 = parseFloat(prompt("Ingrese valor de volaje:"))
             const valor2 = parseFloat(prompt("Ingrese valor de corriente:"))
+            if (isNaN(Number(valor1)) || isNaN(Number(valor2))) {//valor es un numero 
+                alert("Al menos uno de los valores es incorrecto");
+                
+             }  else {
+                const resultado = calcParametro(valor1, valor2 , opcion)
+                console.log("El resultado es: " + resultado)
+                alert("La resistencia calculada es  " + resultado + "(Ω)")}    
 
-            const resultado = calcParametro(valor1, valor2 , opcion)
-            console.log("El resultado es: " + resultado)
-            alert("La resistencia calculada es  " + resultado + "Ohm")
+            
         }
-        else if (opcion === '3') {//opcion Voltaje
+        else if (opcion === '3') {//opcion  corriente
             
             const valor1 = parseFloat(prompt("Ingrese valor de voltaje:"))
             const valor2 = parseFloat(prompt("Ingrese valor de resistencia:"))
-
+            if (isNaN(Number(valor1)) || isNaN(Number(valor2))) {//valor es un numero
+                alert("Al menos uno de los valores es incorrecto");
+                
+             }   else {   
             const resultado = calcParametro(valor1, valor2 , opcion)
             console.log("El resultado es: resultado" + resultado)
-            alert("La corriente calculada es  " + resultado + "A")
+            alert("La corriente calculada es  " + resultado + "(A)")}
 
-        } else if (opcion === '4') {
+        } else if (opcion === '4') {//salida bucle
             console.log("Saliendo del programa. ¡Hasta luego!")
             break;
-        } else {
+        } else {//volver a preguntar
             console.log("Opción no válida. Por favor, elija 1, 2, 3 o 4.")
-            alert("Opcion no valida")
+            alert("Opcion no valida, elija 1, 2, 3 o 4.")
         }
     }
 };
